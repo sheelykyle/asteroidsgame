9e250e58bea363eba10b759bef7a7f80f4d7618e
sheelykyle <kyle@shee.ly>
2025-11-15 10:51:27 -0500

    Added collision detection

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pygame
+import sys
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH 
-from logger import log_state
+from logger import log_event, log_state
 from player import Player
 
 
@@ -32,6 +33,12 @@ def main():
             
         screen.fill("black")
         updatable.update(dt)
+        for object in asteroids:
+            if player.collides_with(object):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+
         for drawn in drawable:
             drawn.draw(screen)
         pygame.display.flip()
