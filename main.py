c8ae132db19b5e7e7225dc0b5a69233c45b1f93f
sheelykyle <kyle@shee.ly>
2025-11-15 10:00:59 -0500

    Added ship, rotation, and movement

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
+import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH 
 from logger import log_state
-import pygame
+from player import Player
 
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
-    screen = pygame.display.set_mode(
-        (SCREEN_WIDTH, SCREEN_HEIGHT)
-        )
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+
     while True:
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
+        
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
